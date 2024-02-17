7ded207fa3edd3b100962778df7431366021c0c3
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-02-17 20:22:19 +0100

    add node tests

--- test/node/node_test.cc
+++ test/node/node_test.cc
@@ -2,7 +2,37 @@
 #include "node_test.h"
 
 namespace node_test {
-TEST_F(NodeTest, True) {
-  EXPECT_EQ(true, true);
+TEST_F(NodeTest, NoNeighborsNode_HasNext_ReturnsFalse) {
+  EXPECT_FALSE(no_neighbors_node_.HasNext());
+}
+
+TEST_F(NodeTest, NoNeighborsNode_HasPrevious_ReturnsFalse) {
+  EXPECT_FALSE(no_neighbors_node_.HasPrevious());
+}
+
+TEST_F(NodeTest, NoNeighborsNodeAfter_SetNext_HasNextReturnsTrue) {
+  auto ptr = std::make_shared<node::Node<int>>(5);
+  no_neighbors_node_.SetNext(ptr);
+  EXPECT_TRUE(no_neighbors_node_.HasNext());
+}
+
+TEST_F(NodeTest, NoNeighborsNodeAfter_SetPrevious_HasPreviousReturnsTrue) {
+  auto ptr = std::make_shared<node::Node<int>>(5);
+  no_neighbors_node_.SetPrevious(ptr);
+  EXPECT_TRUE(no_neighbors_node_.HasPrevious());
+}
+
+TEST_F(NodeTest,
+       NoNeighborsNodeAfterSetNext_GetNext_ReturnsExpectedValue) {
+  auto ptr = std::make_shared<node::Node<int>>(5);
+  no_neighbors_node_.SetNext(ptr);
+  EXPECT_EQ(no_neighbors_node_.GetNextNode(), ptr);
+}
+
+TEST_F(NodeTest,
+       NoNeighborsNodeAfterSetPrevious_GetPrevious_ReturnsExpectedValue) {
+  auto ptr = std::make_shared<node::Node<int>>(5);
+  no_neighbors_node_.SetPrevious(ptr);
+  EXPECT_EQ(no_neighbors_node_.GetPreviousNode(), ptr);
 }
 }  // namespace node_test
--- test/node/node_test.h
+++ test/node/node_test.h
@@ -2,11 +2,12 @@
 
 #include <gtest/gtest.h>
 #include <string>
-#include "linked_list.h"
+#include "node.h"
 
 namespace node_test {
 class NodeTest : public ::testing::Test {
  protected:
+  node::Node<int> no_neighbors_node_ = node::Node(10);
   void SetUp() override {}
   void TearDown() override {}
 };
