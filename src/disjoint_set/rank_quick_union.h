919668ff9b5df8cbb897a15474152d5926b89a8f
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-03-08 16:13:31 +0100

    remove code duplication in RankQuickUnion

--- src/disjoint_set/rank_quick_union.h
+++ src/disjoint_set/rank_quick_union.h
@@ -2,32 +2,21 @@
 
 #include <algorithm>
 #include <numeric>
-#include "disjoint_set.h"
+#include "weight_quick_union.h"
 
 namespace disjoint_set {
 
 template <typename T>
-class RankQuickUnion : public DisjointSet<T> {
+class RankQuickUnion : public WeightQuickUnion<T> {
  private:
-  T IsNotRoot(T element) { return this->blocks_.at(element) >= 0; };
-  T GetFather(T element) { return this->blocks_.at(element); };
   T GetRootRank(T root) { return this->blocks_.at(root) * -1; }
 
  public:
-  RankQuickUnion(T size) : DisjointSet<T>(size){};
-
-  T FindBlock(T element) override {
-    auto element_root = element;
-
-    while (IsNotRoot(element_root))
-      element_root = GetFather(element_root);
-
-    return element_root;
-  };
+  RankQuickUnion(T size) : WeightQuickUnion<T>(size){};
 
   void MergeBlocks(T first_block, T second_block) override {
-    auto first_block_root = FindBlock(first_block),
-         second_block_root = FindBlock(second_block),
+    auto first_block_root = this->FindBlock(first_block),
+         second_block_root = this->FindBlock(second_block),
          first_block_root_rank = GetRootRank(first_block_root),
          second_block_root_rank = GetRootRank(second_block_root);
 
