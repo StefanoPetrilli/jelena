d823769af9516b9a195217bb5783e7efe4d5c17a
StefanoPetrilli <stefanop_1999@hotmail.it>
2024-05-18 10:07:40 +0200

    Working memory packed arrays

--- src/array/packed_memory_array.h
+++ src/array/packed_memory_array.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <bit>
 
 namespace array {
 
@@ -25,6 +26,10 @@ class PackedMemoryArray {
     }
     return lg2;
   }
+
+  // int Log2(uint32_t n) {
+  //   return std::bit_width(n) - 1;
+  // }
 
   double GetUpperTreshold(uint32_t level) {
     return 1.0 - ((1.0 - 0.5) * level) / (double)Log2(this->capacity_);
@@ -60,8 +65,13 @@ class PackedMemoryArray {
     if (left_block == right_block)
       return left_block;
 
-    throw std::logic_error("Not implemented");
-    return 1;
+    uint32_t middle_block = left_block + ((right_block - left_block) / 2);
+    ContentType highest_middle = GetMaximumFromBlock(middle_block);
+
+    if (value < highest_middle)
+      return FindBlock(value, 0, middle_block);
+
+    return FindBlock(value, middle_block + 1, right_block);
   }
 
   uint32_t FindBlock(ContentType value) {
@@ -70,12 +80,12 @@ class PackedMemoryArray {
 
     uint32_t middle_block = (this->block_count_ - 1) / 2;
 
-    if (middle_block == this->block_count_ - 1) return 0;
+    if (middle_block == this->block_count_ - 1)
+      return 0;
 
-    ContentType highest_middle = GetMaximumFromBlock(middle_block),
-                lowest_left = GetMinimumFromBlock(0);
+    ContentType highest_middle = GetMaximumFromBlock(middle_block);
 
-    if (value > lowest_left && value < highest_middle)
+    if (value < highest_middle)
       return FindBlock(value, 0, middle_block);
 
     return FindBlock(value, middle_block + 1, this->block_count_ - 1);
@@ -94,9 +104,16 @@ class PackedMemoryArray {
     auto position = std::ranges::lower_bound(auxiliary_vector, value);
     auxiliary_vector.insert(position, value);
 
+    //FIXME(StefanoPetrilli): make it add the spaces distributed evenly
+    for (size_t i = 1; auxiliary_vector.size() < this->block_size_; i += 2) {
+      auxiliary_vector.insert(
+          auxiliary_vector.begin() + std::min(i, auxiliary_vector.size()), 0);
+    }
+
     for (uint32_t i = 0; i < auxiliary_vector.size(); ++i) {
       this->content_[initial_position + i] = auxiliary_vector[i];
     }
+
     ++this->elements_count_;
   }
 
@@ -114,24 +131,28 @@ class PackedMemoryArray {
     return elements_in_block < this->block_size_;
   }
 
-  bool OtherBlocksHaveSpace(uint32_t block_number, uint32_t level) {
-    if (level > this->level_count_)
-      return false;
+  // FIXME(StefanoPetrilli) make this readable once it's clear that it works
+  uint32_t OtherBlocksHaveSpace(uint32_t block_number, uint32_t level = 1) {
+    if (level > this->level_count_ + 1)
+      return 0;
 
     auto upper_threshold = GetUpperTreshold(level);
-    uint32_t search_window = level * this->block_size_;
+    uint32_t denomiantor = 1 << (level - 1);
+    uint32_t normalized_block_number = block_number / denomiantor;
+    uint32_t normalized_block_size = this->block_size_ << (level - 1);
 
     auto block_start =
-        std::next(this->content_.begin(), block_number * this->block_size_);
-    auto block_end = std::next(block_start, search_window);
+        std::next(this->content_.begin(),
+                  normalized_block_number * normalized_block_size);
+    auto block_end = std::next(block_start, normalized_block_size);
 
     auto element_count = std::ranges::count_if(
         block_start, block_end, [](auto value) { return value != 0; });
 
-    auto density = (element_count + 1) / search_window;
-    if (density < upper_threshold)
-      return true;
-    OtherBlocksHaveSpace(block_number, level + 1);
+    auto density = element_count / static_cast<float>(normalized_block_size);
+    if (density <= upper_threshold)
+      return level;
+    return OtherBlocksHaveSpace(block_number, level + 1);
   }
 
   void IncreaseSize() {
@@ -141,7 +162,7 @@ class PackedMemoryArray {
 
     uint32_t control = 0;
     for (uint32_t i = 0; i < this->content_.size(); ++i) {
-      if (this->content_.at(0)) {
+      if (this->content_.at(i)) {
         int idx = density * (control++);
         temp_content.at(idx) = this->content_.at(i);
       }
@@ -149,6 +170,36 @@ class PackedMemoryArray {
 
     this->content_.swap(temp_content);
     this->UpdateVars(new_capacity);
+  }
+
+  // FIXME(StefanoPetrilli) make this readable once it's celar that it works
+  void RebalanceInterval(uint32_t block_number, uint32_t level) {
+    uint32_t denomiantor = 1 << (level - 1);
+    uint32_t normalized_block_number = block_number / denomiantor;
+    uint32_t normalized_block_size = this->block_size_ << (level - 1);
+
+    std::vector<ContentType> auxiliary_vector;
+    auxiliary_vector.reserve(normalized_block_size);
+
+    auto initial_position = normalized_block_number * normalized_block_size;
+
+    for (auto i = initial_position;
+         i < initial_position + normalized_block_size; ++i) {
+      if (this->content_.at(i) != 0)
+        auxiliary_vector.push_back(content_.at(i));
+    }
+
+    for (size_t i = 1; auxiliary_vector.size() < normalized_block_size;
+         i += 2) {
+      auxiliary_vector.insert(auxiliary_vector.begin() + i, 0);
+      if (auxiliary_vector.size() >= normalized_block_size)
+        break;
+      auxiliary_vector.insert(auxiliary_vector.end() - i, 0);
+    }
+
+    for (uint32_t i = 0; i < auxiliary_vector.size(); ++i) {
+      this->content_[initial_position + i] = auxiliary_vector[i];
+    }
   }
 
  public:
@@ -161,12 +212,15 @@ class PackedMemoryArray {
     auto block_number = FindBlock(value);
 
     if (BlockHasSpace(block_number)) {
-      Insert(value, block_number);
+      this->Insert(value, block_number);
       return;
     }
 
-    if (OtherBlocksHaveSpace(block_number, 2)) {
-      throw std::logic_error("Not implemented");
+    auto space_at_level = OtherBlocksHaveSpace(block_number);
+    if (space_at_level) {
+      this->RebalanceInterval(block_number, space_at_level);
+      this->Insert(value, block_number);
+      return;
     }
 
     this->IncreaseSize();
--- test/array/packed_memory_array_test.cc
+++ test/array/packed_memory_array_test.cc
@@ -31,7 +31,7 @@ TEST_F(PackedMemoryArrayTest,
   pma.Insert(50);
   pma.Insert(70);
   auto actual = pma.ToString();
-  std::string expected = "50,70,80, ";
+  std::string expected = "50, ,70,80";
   EXPECT_EQ(actual, expected);
 }
 
@@ -55,7 +55,7 @@ TEST_F(PackedMemoryArrayTest,
   pma.Insert(65);
 
   auto actual = pma.ToString();
-  std::string expected = "50,65,70, ,80, ,90, ";
+  std::string expected = "50, ,65,70,80, ,90, ";
   EXPECT_EQ(actual, expected);
 }
 
@@ -69,7 +69,143 @@ TEST_F(PackedMemoryArrayTest,
   pma.Insert(85);
 
   auto actual = pma.ToString();
-  std::string expected = "50,65,70, ,80,85,90, ";
+  std::string expected = "50, ,65,70,80, ,85,90";
   EXPECT_EQ(actual, expected);
 }
+
+TEST_F(PackedMemoryArrayTest,
+       AfterInserting7Values_Returns_OrderedValuesString) {
+  pma.Insert(80);
+  pma.Insert(50);
+  pma.Insert(70);
+  pma.Insert(90);
+  pma.Insert(65);
+  pma.Insert(85);
+  pma.Insert(10);
+
+  auto actual = pma.ToString();
+  std::string expected = "10,50,65,70,80, ,85,90";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       AfterInserting8Values_Returns_OrderedValuesString) {
+  pma.Insert(80);
+  pma.Insert(50);
+  pma.Insert(70);
+  pma.Insert(90);
+  pma.Insert(65);
+  pma.Insert(85);
+  pma.Insert(10);
+  pma.Insert(21);
+
+  auto actual = pma.ToString();
+  std::string expected = "10, ,21, ,50, ,65,70,80, ,85, ,90, , , ";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       AfterInsertingValues_Returns_OrderedValuesString) {
+  pma.Insert(80);
+  pma.Insert(50);
+  pma.Insert(70);
+  pma.Insert(90);
+  pma.Insert(65);
+  pma.Insert(85);
+  pma.Insert(10);
+  pma.Insert(21);
+  pma.Insert(30);
+  pma.Insert(20);
+  pma.Insert(5);
+  pma.Insert(13);
+
+  auto actual = pma.ToString();
+  std::string expected = "5, ,10, ,13,20,21,30,50,65,70,80, ,85, ,90";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       AfterInsertingValues_Returns_OrderedValuesString2) {
+  pma.Insert(80);
+  pma.Insert(50);
+  pma.Insert(70);
+  pma.Insert(90);
+  pma.Insert(65);
+  pma.Insert(85);
+  pma.Insert(10);
+  pma.Insert(100);
+  pma.Insert(110);
+  pma.Insert(120);
+  pma.Insert(95);
+  pma.Insert(105);
+  pma.Insert(125);
+
+  auto actual = pma.ToString();
+  std::string expected = "10, ,50, ,65,70,80,85,90, ,95,100,105,110,120,125";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       AfterInsertingValues_Returns_OrderedValuesString3) {
+  pma.Insert(80);
+  pma.Insert(50);
+  pma.Insert(70);
+  pma.Insert(90);
+  pma.Insert(65);
+  pma.Insert(85);
+  pma.Insert(10);
+  pma.Insert(21);
+  pma.Insert(30);
+  pma.Insert(20);
+  pma.Insert(5);
+  pma.Insert(13);
+  pma.Insert(100);
+  pma.Insert(95);
+  pma.Insert(97);
+
+  auto actual = pma.ToString();
+  std::string expected =
+      "5, ,10, ,13, ,20, ,21, ,30, ,50, ,65, ,70, ,80, ,85, ,90, ,95, ,97, "
+      ",100, , , ";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       ForCycleInsert100_Returns_OrderedValuesString) {
+  for (size_t i = 0; i <= 100; i++)
+    pma.Insert(i);
+
+  auto actual = pma.ToString();
+  std::string expected =
+      "1, ,2, ,3, ,4, ,5, ,6, ,7, ,8, ,9, ,10, ,11, ,12, ,13, ,14, ,15, ,16, "
+      ",17, ,18, ,19, ,20, ,21, ,22, ,23, ,24, ,25, ,26, ,27, ,28, ,29, ,30, "
+      ",31, ,32, ,33, ,34, ,35, ,36, ,37, ,38, ,39, ,40, ,41, ,42, ,43, ,44, "
+      ",45, ,46, ,47, ,48, ,49, ,50, ,51, ,52, ,53, ,54, ,55, ,56, ,57, ,58, "
+      ",59, ,60, ,61, ,62, ,63, ,64, ,65, ,66, ,67, ,68, ,69, ,70, ,71, ,72, "
+      ",73, ,74, ,75, ,76, ,77, ,78, ,79, ,80, ,81, ,82, ,83, ,84, ,85, ,86, "
+      ",87, ,88, ,89, ,90, ,91, ,92, , , , , , , , , , , , , , , , , , , , , , "
+      ", , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , "
+      ",93, ,94, ,95, ,96, ,97, ,98, ,99, ,100, ";
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_F(PackedMemoryArrayTest,
+       ReverseForCycleInsert100_Returns_OrderedValuesString) {
+  for (size_t i = 101; i >= 1; i--)
+    pma.Insert(i);
+
+  auto actual = pma.ToString();
+  std::string expected =
+      "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22, ,23, ,24, "
+      ",25, ,26, ,27, ,28, , ,29, , ,30, , ,31, , ,32, , ,33, , ,34, , ,35, , "
+      ",36, , ,37, , ,38, , ,39, , ,40, , ,41, , ,42, , ,43, , ,44, , ,45, , "
+      ",46, , ,47, , ,48, , ,49, , ,50, , ,51, , ,52, , ,53, , ,54, , ,55, , "
+      ",56, , ,57, , ,58, , ,59, , ,60, , ,61, , ,62, , ,63, , ,64, , ,65, , "
+      ",66, , ,67, , ,68, , ,69, , ,70, , ,71, , ,72, , ,73, , ,74, , ,75, , "
+      ",76, , ,77, , ,78, , ,79, , ,80, , ,81, , ,82, , ,83, , ,84, , ,85, , "
+      ",86, , ,87, , ,88, , ,89, , ,90, , ,91, , ,92, , ,93, , ,94, , ,95, , "
+      ",96, , ,97, , ,98, , ,99, , ,100, , ,101, , , ";
+  EXPECT_EQ(actual, expected);
+}
+
 }  // namespace array_test
